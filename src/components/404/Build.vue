c9c3c2eae4bcd3e8993664293f2f33fb4593cf7e
Rohman Abdul Azis <rohmanaa@akses.live>
2024-02-20 16:13:19 +0700

    404 Page

--- src/components/404/Build.vue
+++ src/components/404/Build.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="px-4 py-5 my-5 text-center">
-        <div class="appie-error-area ">
+        <div class="appie-error-area">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-12">
                         <div class="appie-error-content ">
-                            <img src="@/assets/images/error/building.svg" alt="" style="width:300px">
-                            
-                            <h3 class="title">Maaf!, Halaman sedang dalam perbaikan</h3>
-                        
+                            <img src="@/assets/images/error/building.svg" alt="" style="width:450px">
+                            <span>Mohon maaf !</span>
+                            <h3 class="title">Halaman sedang dibangun</h3>
                         </div>
                     </div>
                 </div>
--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,15 +1,42 @@
 <template>
-  <error-com/>
+
+<section class="bg-error d-flex justify-content-center align-items-center">
+    <div class="py-5 text-center">
+        <div class="appie-error-area">
+            <div class="container">
+                <div class="col-lg-12">
+                    <div class="appie-error-content">
+                        <img src="@/assets/images/error/404.svg" alt="" style="width:450px">
+                        <span>Mohon maaf !</span>
+                        <h3 class="title">Halaman yang sedang Anda cari tidak tersedia</h3>
+
+                        <router-link to="/" class="btn btn-info text-white" size="sm">Kembali Ke Beranda<i class="fal fa-arrow-right ms-2"></i></router-link>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
 </template>
 
 <script>
-import ErrorCom from '../components/404/ErrorCom.vue'
 export default {
-	components: { ErrorCom },
+    components: {},
+    data() {
+        return {
+            sidebar: false,
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
 
 }
 </script>
-
-<style>
-
-</style>
